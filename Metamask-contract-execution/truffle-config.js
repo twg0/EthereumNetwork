55ad199874e3aad54485026286fabc9f9961cde8
twg0 <fruitsjh@gmail.com>
2024-04-13 23:36:43 +0900

    feat: truffle-config.js 생성

---
+++ Metamask-contract-execution/truffle-config.js
@@ -0,0 +1,12 @@
+/* 에러 표시 때문에 주석처리
+
+networks: {
+ 
+    development: {
+        host: '127.0.0.1', // Localhost (default: none)
+        port: 8545, // Standard Ethereum port (default: none)
+        network_id: '*', // Any network (default: none)
+    }
+ 
+}
+*/
