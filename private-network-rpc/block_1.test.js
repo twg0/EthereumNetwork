bc3f984c8ab220075e38aa0e78aa9849de802d34
twg0 <fruitsjh@gmail.com>
2024-04-13 22:55:16 +0900

    feat: block_1.test.js 생성

---
+++ private-network-rpc/block_1.test.js
@@ -0,0 +1,14 @@
+// block.test.js 파일
+ 
+const Web3 = require("web3");
+ 
+describe("Block test", () => {
+  let web3;
+ 
+  it("web3 연결 테스트", async () => {
+    web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9000"));
+ 
+    const block_number = await web3.eth.getBlockNumber();
+    console.log("블록 number : ", block_number);
+  });
+});
