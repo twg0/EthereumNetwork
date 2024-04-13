83c2484db113dcbbbeae5e48e6d8847f567fd03d
twg0 <fruitsjh@gmail.com>
2024-04-13 23:11:31 +0900

    feat: truffle-config.js 생성

---
+++ Truffle/truffle-config.js
@@ -0,0 +1,16 @@
+/* truffle-config.js 파일 */
+
+/* 에러표시 때문에 주석처리
+
+networks: {
+    
+    development: {
+        host: '127.0.0.1', // Localhost (default: none)
+        port: 9005, // Standard Ethereum port (default: none)
+        network_id: '*', // Any network (default: none)
+        websockets: true,
+    },
+ 
+}
+
+*/
